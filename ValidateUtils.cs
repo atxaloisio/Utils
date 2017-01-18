d2e29481db310607d211ef30b229abe587a53879
Aloisio Xavier <atxaloisio@gmail.com>
2017-01-18 17:03:52 -0200

    inclusão de arquivos

---
+++ ValidateUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public static class ValidateUtils
+    {
+        public static bool isDate(string data)
+        {
+            Regex r = new Regex(@"(\d{2}\/\d{2}\/\d{4})");
+            if (r.Match(data).Success)
+            {
+                DateTime Temp;
+
+
+                if (DateTime.TryParse(data, out Temp) == true)
+                    return true;
+                else
+                    return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool isDateTime(string data)
+        {
+            Regex r = new Regex(@"(\d{2}\/\d{2}\/\d{4} \d{2}:\d{2})");
+            return r.Match(data).Success;
+        }
+    }
+}
