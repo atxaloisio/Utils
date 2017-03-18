1fbc1ae4bf541b8b091ee270011e6efe664d8111
Aloisio Xavier <atxaloisio@gmail.com>
2017-03-18 03:25:29 -0300

    salvando alterações

---
+++ TrataException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public static class TrataException
+    {
+        public static string getAllMessage(Exception ex)
+        {
+            string retorno = string.Empty;
+
+            retorno = ex.Message;
+
+            if (ex.InnerException != null)
+            {
+                retorno += "\n" + getAllMessage(ex.InnerException);
+            }
+
+            return retorno;
+        }
+    }
+}
