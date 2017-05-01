e064020917a4aa06048f09344ad25e975d8fe57a
atxaloisio <atxaloisio@hotmail.com>
2017-05-01 18:39:26 -0300

    salvando alterações.

---
+++ Parametro.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace Utils
+{
+    public static class Parametro
+    {
+        public static string GetParametro(string chave)
+        {
+            string ConnectionString = ConfigurationManager.ConnectionStrings["prjbase.Properties.Settings.dbintegracaoConnectionString"].ConnectionString;            
+            string retorno = string.Empty;
+            MySqlConnection con = new MySqlConnection(ConnectionString);
+            try
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT valor from parametro where chave = ?chave", con);
+                cmd.Parameters.AddWithValue("?chave", chave);
+                
+                MySqlDataReader dr = cmd.ExecuteReader();
+                dr.Read();
+                if (dr.HasRows)
+                {
+                    retorno = dr.GetString("valor");
+                }
+                dr.Close();
+                dr.Dispose();
+                cmd.Dispose();
+                return retorno;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }            
+        }
+        
+        public static void SetParametro(string chave, string valor)
+        {
+            string ConnectionString = ConfigurationManager.ConnectionStrings["prjbase.Properties.Settings.dbintegracaoConnectionString"].ConnectionString;
+            string retorno = string.Empty;
+            MySqlConnection con = new MySqlConnection(ConnectionString);
+            try
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand("update parametro set valor=?valor where chave = ?chave", con);
+                cmd.Parameters.AddWithValue("?chave", chave);
+                cmd.Parameters.AddWithValue("?valor", valor);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();                
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }
+        }
+
+        public static void AddParametro(string chave, string valor)
+        {
+            string ConnectionString = ConfigurationManager.ConnectionStrings["prjbase.Properties.Settings.dbintegracaoConnectionString"].ConnectionString;
+            string retorno = string.Empty;
+            MySqlConnection con = new MySqlConnection(ConnectionString);
+            try
+            {
+                con.Open();                
+                MySqlCommand cmd = new MySqlCommand("insert into parametro (chave,valor) values(?chave,?valor)", con);
+                cmd.Parameters.AddWithValue("?chave", chave);
+                cmd.Parameters.AddWithValue("?valor", valor);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }
+        }
+    }
+}
--- Sequence.cs
+++ Sequence.cs
@@ -27,7 +27,11 @@ namespace Utils
                 MySqlCommand cmd = new MySqlCommand("SELECT nextval('" + Sequence + "') as nextsequence", con);
                 MySqlDataReader dr = cmd.ExecuteReader();
                 dr.Read();
-                retorno = dr.GetInt64("nextsequence");                
+                if (dr.HasRows)
+                {
+                    retorno = dr.GetInt64("nextsequence");
+                }
+                
                 dr.Close();
                 dr.Dispose();
                 cmd.Dispose();
@@ -46,16 +50,24 @@ namespace Utils
 
         public static long GetCurrentVal(string Sequence)
         {
-            string ConnectionString = ConfigurationManager.ConnectionStrings["MySQLEntities_local"].ConnectionString;
+            string ConnectionString = ConfigurationManager.ConnectionStrings["prjbase.Properties.Settings.dbintegracaoConnectionString"].ConnectionString;
             long retorno = 0;
             MySqlConnection con = new MySqlConnection(ConnectionString);
             try
             {
                 con.Open();
-                MySqlCommand cmd = new MySqlCommand("SELECT currentval('" + Sequence + "') as currentsequence", con);
+                MySqlCommand cmd = new MySqlCommand("SELECT currentval(?Sequence) as currentsequence", con);
+                cmd.Parameters.AddWithValue("?Sequence", Sequence);
                 MySqlDataReader dr = cmd.ExecuteReader();
                 dr.Read();
-                retorno = dr.GetInt64("currentsequence");
+                if (dr.HasRows)
+                {
+                    if (!dr.IsDBNull(0))
+                    {
+                        retorno = dr.GetInt64("currentsequence");
+                    }
+                    
+                }
                 dr.Close();
                 dr.Dispose();
                 cmd.Dispose();
@@ -74,12 +86,14 @@ namespace Utils
 
         public static void SetCurrentVal(string Sequence, long Value)
         {
-            string ConnectionString = ConfigurationManager.ConnectionStrings["MySQLEntities_local"].ConnectionString;            
+            string ConnectionString = ConfigurationManager.ConnectionStrings["prjbase.Properties.Settings.dbintegracaoConnectionString"].ConnectionString;
             MySqlConnection con = new MySqlConnection(ConnectionString);
             try
             {
                 con.Open();
-                MySqlCommand cmd = new MySqlCommand("UPDATE sequence_data SET sequence_cur_value = "+ Convert.ToString(Value)+ " WHERE sequence_name = " + Sequence, con);
+                MySqlCommand cmd = new MySqlCommand("UPDATE sequence_data SET sequence_cur_value = ?Value WHERE sequence_name = ?Sequence", con);
+                cmd.Parameters.AddWithValue("?Sequence", Sequence);
+                cmd.Parameters.AddWithValue("?Value", Value);
                 cmd.ExecuteNonQuery();                
                 cmd.Dispose();
                 
